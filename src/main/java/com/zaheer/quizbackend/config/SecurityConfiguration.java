48330cd09b4592e04c6a88122fc681a633367caf
Ivan Kolanovic <ik3130@g.rit.edu>
2022-04-25 05:01:40 +0200

    up

--- src/main/java/com/zaheer/quizbackend/config/SecurityConfiguration.java
+++ src/main/java/com/zaheer/quizbackend/config/SecurityConfiguration.java
@@ -34,7 +34,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    //    http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     http.csrf()
+        .and()
+        .cors()
         .disable()
         .authorizeRequests()
         .antMatchers("/quiz/auth/login")
