8b6f5f89610512cea9bc66a7a3604f75a3f3346e
Ivan Kolanovic <ik3130@g.rit.edu>
2022-04-26 20:59:16 +0200

    wss

--- src/main/java/com/zaheer/quizbackend/config/SecurityConfiguration.java
+++ src/main/java/com/zaheer/quizbackend/config/SecurityConfiguration.java
@@ -20,8 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -65,9 +64,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowedOrigins(Collections.singletonList("*"));
+    configuration.setAllowedMethods(Collections.singletonList("*"));
+    configuration.setAllowedHeaders(Collections.singletonList("*"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
--- src/main/java/com/zaheer/quizbackend/config/WebSocketConfig.java
+++ src/main/java/com/zaheer/quizbackend/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/wss").setAllowedOriginPatterns("*");
+    registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
   }
 }
