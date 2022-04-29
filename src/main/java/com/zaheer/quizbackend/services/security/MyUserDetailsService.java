0090f9a68ffa8c666208d43e27df6efa29823aa8
Marko Milic Bucevic <mm7026@g.rit.edu>
2022-04-29 17:33:36 +0200

    use findByEmail instead of findByEmailAndActive

--- src/main/java/com/zaheer/quizbackend/services/security/MyUserDetailsService.java
+++ src/main/java/com/zaheer/quizbackend/services/security/MyUserDetailsService.java
@@ -19,7 +19,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    Optional<User> user = userRepository.findByEmailAndActiveTrue(email);
+    Optional<User> user = userRepository.findByEmail(email);
     user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + email));
     return user.map(MyUserDetails::new).get();
   }
